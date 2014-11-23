303d3c0505ed5da95bc857bb9f8ec8d78f1f6e22
Oz <oss202@gmail.com>
2014-11-23 14:42:02 +0000

    Parcelable class

--- app/src/main/java/com/ozsaat/reader/ui/reader/NewsItemActivity.java
+++ app/src/main/java/com/ozsaat/reader/ui/reader/NewsItemActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ozsaat.reader.R;
 import com.ozsaat.reader.rss.RssItem;
+import com.squareup.picasso.Picasso;
 
 public class NewsItemActivity extends Activity {
     public static final String EXTRA_RSS_ITEM = "EXTRA_RSS_ITEM";
@@ -24,6 +27,17 @@ public class NewsItemActivity extends Activity {
 
         RssItem rssItem = getIntent().getParcelableExtra(EXTRA_RSS_ITEM);
 
+        ImageView photoImageView = (ImageView) findViewById(R.id.photoImageView);
+        TextView titleTextView = (TextView) findViewById(R.id.titleTextView);
+        TextView authorTextView = (TextView) findViewById(R.id.authorTextView);
+        TextView dateTextView = (TextView) findViewById(R.id.dateTextView);
+
+        titleTextView.setText(rssItem.getTitle());
+        dateTextView.setText(rssItem.getDate());
+        authorTextView.setText(rssItem.getAuthor());
+        Picasso.with(this).load(rssItem.getThumbnail()).fit().centerCrop().into(photoImageView);
 
     }
+
+
 }
